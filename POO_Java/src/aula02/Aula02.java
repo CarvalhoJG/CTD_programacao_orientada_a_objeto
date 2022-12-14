159de99c4d6475cfc49e8d9c487ecf281e69c1ae
Julia Carvalho <jucarvalhoapps@gmail.com>
2022-12-13 22:42:56 -0300

    Aula 02 estrutura de decisão e repetição.

---
+++ POO_Java/src/aula02/Aula02.java
@@ -0,0 +1,52 @@
+package aula02;
+
+public class Aula02 {
+
+
+    /*
+     *Estrutura de Decisão:
+     * Baseado em uma condição nosso programa vai tomar uma decisão. Exemplos:
+     * if(consição){ Se(if)(condição a qual vai se basear para tomar a decisão)
+     *   //código que é executado se a condição for verdadeira.
+     *}else{ senao(else)
+     *   //Código que se executa quando a condição for verdadeira.
+     *}
+     *
+     *Switch(variavel){Analisa a variavel e trocar(switch) o caso ate atender a condição
+     *   case valor1: caso a variavel tiver 1
+     *       codigo a ser executado
+     *   break; parada
+     *
+     *   case valor2: caso a variavel tiver 1
+     *       codigo a ser executado
+     *   break; parada
+     *
+     *   default;
+     *       codigo a ser executado caso a variavel não estive rlistadas em nenhum dos casos.
+     *}
+     *
+     *
+     *Estrutura de Repetição
+     * baseado em uma condição ele vai ficar se repetindo.
+     *
+     * for(int i = 0; i < condiçõa; i++){ para(for) (variavel do tipo inteira recebe o valor 0; veridica se i é menor que o valor maximo; acrescenta +1){
+     *   codigo a executar ate que a condiçõo seja atendida
+     *}
+     *
+     *for(Object object: listaObjetos){ definimos um tipo de objeto para cada lista do tipo ele vai executar uma instrução
+     *   codigo a ser executado para objeto da lista
+     *}
+     *
+     *while(condição){equanto(while) a condição for verdadeira
+     *   codigo a ser executado caso a condição for verdadeira
+     *}
+     *
+     * A diferença entre as estruturas de repetição:
+     *
+     *for executa ate atingir a condição.
+     *forit executa para todos da lista, se a lista conter dois objetos ele irá executra duas vezes.
+     *While só executa enquanto a condição for verdadeira.
+     *
+     */
+
+}
